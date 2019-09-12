dced6c3277f3136b2bbcf8dee6b7af3f95820798
Richard Murray <rfm@yorku.ca>
2019-09-12 09:16:24 -0400

    minor edits

--- 01 - overview; matrices/code2_matrices.m
+++ 01 - overview; matrices/code2_matrices.m
@@ -8,17 +8,17 @@
 % - many uses for matrices:  trial-by-trial data, images, etc.
 % - related terms:  row vector (1 x m), column vector (n x 1), scalar (1 x 1)
 
-a = [ 1 2 3 ; 4 5 6 ]   % enter values manually
+a = [ 1 2 3 ; 4 5 6 ]      % enter values manually
 a = [ 1 ; 2 ; 3 ]
-a = 1:10                % sequence
-a = 1:2:10              % sequence
-a = linspace(1,5,20)    % sequence
-a = zeros(3,5)          % matrix of zeros
-a = ones(3,5)           % matrix of ones
-a = rand(3,5)           % uniform random values
-a = randn(3,5)          % normal random values
-a = []                  % empty matrix
-a = rand(3,3,2)         % three dimensional matrix
+a = 1:10                   % sequence
+a = 1:2:10                 % sequence
+a = linspace( 1, 5, 20 )   % sequence
+a = zeros( 3, 5 )          % matrix of zeros
+a = ones( 3, 5 )           % matrix of ones
+a = rand( 3, 5 )           % uniform random values
+a = randn( 3, 5 )          % normal random values
+a = []                     % empty matrix
+a = rand( 3, 3, 2 )        % three dimensional matrix
 
 a = [ 4 8 9 ];
 b = [ 2 3 ];
@@ -27,7 +27,7 @@ c = [ a b ]   % combine matrices to form a new matrix
 
 %% mathematical operations on matrices
 
-a = rand(3);
+a = rand( 3 );
 b = 3 * a        % scalar multiplication
 c = a + b        % matrix addition
 c = a * b        % matrix multiplication
@@ -38,27 +38,27 @@ c = 10 * a + 3   % scalar multiplication and addition
 
 %% matrix related functions
 
-a = rand(3,5);
-dim = size(a)
-[m,n] = size(a)
-k = numel(a)
-b = reshape(a,[ 1 15 ])
-b = repmat(a,[ 2 3 ])
+a = rand( 3, 5 );
+dim = size( a )
+[ m, n ] = size( a )
+k = numel( a )
+b = reshape( a, [ 1 15 ] )  % note:  MATLAB functions never change the values of their input arguments
+b = repmat( a, [ 2 3 ] )
 
 
 %% subscript indices
 
-a = rand(5);
-b = a(1,2)          % get an element using subscripts
-b = a(1,[ 2 3 4 ])  % get multiple elements using multiple subscripts
-b = a(1,2:4)        % get multiple elements using a range
-b = a(1,:)          % get a whole row
-b = a(1,3:end)      % get the last part of a row
-b = a(1:2,2:4)      % get a submatrix
+a = rand( 5 );
+b = a( 1, 2 )          % get an element using subscripts
+b = a( 1, [ 2 3 4 ] )  % get multiple elements using multiple subscripts
+b = a( 1, 2:4 )        % get multiple elements using a range
+b = a( 1, : )          % get a whole row
+b = a( 1, 3:end )      % get the last part of a row
+b = a( 1:2, 2:4 )      % get a submatrix
 
-a(1,[ 2 3 4 ]) = 0  % any of the above can also appear on the left side of
-                    % an equation, in which case new values are assigned to
-                    % those elements
+a( 1, [ 2 3 4 ] ) = 0  % any of the above can also appear on the left side of
+                       % an equation, in which case new values are assigned to
+                       % those elements
 
 
 %% exercises
