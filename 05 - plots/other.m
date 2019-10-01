a3b1d3085ecb1b9a4813688152997e0c477f2b76
Richard Murray <rfm@yorku.ca>
2019-10-01 11:06:41 -0400

    posted lecture topic 5

---
+++ 05 - plots/other.m
@@ -0,0 +1,98 @@
+%% other.m  A few loose ends
+
+%% input and output
+
+% get a number from user's input
+r = input('prompt:  ');
+
+% get a string from user's input
+r = input('prompt:  ','s');
+
+% show the value of a variable
+r = rand;
+disp(r)
+
+% fprintf
+a = rand;
+b = 'abc';
+fprintf('the value of a is %f.  the value of b is %s\n',a,b);
+% numbers:  %-10.6f
+% string:   %10s
+% special characters:  \n  \t  \\  %%  ''
+
+% vectorized fprintf
+a = rand(1,3);
+fprintf('the random numbers are %.2f, %.2f, and %.2f\n',a);
+
+
+%% write some text to a file
+
+% open the file
+fid = fopen('testfile.txt','w');
+% to append to an existing file, use 'a' instead of 'w'
+
+% write a line to the file
+fprintf(fid,'%% %s\n',datestr(now));
+% *** sometimes Windows needs \r\n instead of \n
+
+% write some more lines to the file
+for i = 1:10
+    fprintf(fid,'%.0f\t%2.0f\t%.9f\n',i,round(100*rand),now);
+end
+
+% close the file
+fclose(fid);
+
+% read the file into the workspace
+d = load('testfile.txt','-ascii');
+
+
+%% saving variables
+
+a = rand;
+b = rand;
+c = rand;
+
+% .mat files
+save mydata.mat a
+save mydata.mat b -append
+save mydata.mat
+
+clear
+load mydata.mat
+
+% .txt files
+a = rand(3,5);
+save mydata.txt a -ascii
+clear
+r = load('mydata.txt');
+
+
+%% switch
+
+% switch
+switch quiz
+	case 10
+		grade = 1;
+	case { 8, 9 }
+		grade = 2;
+	case 7
+		grade = 3;
+	otherwise
+		grade = 4;
+end
+
+
+%% debugging
+
+% keyboard, dbstep, dbcont, dbquit
+% - can also set breakpoints in the editor
+
+% echo on/off
+
+
+%% warnings and errors
+
+warning('the file exists but it is empty');
+
+error('unable to open the file');
---
+++ 05 - plots/plots.m
@@ -0,0 +1,121 @@
+%% plots.m  Demonstration code for MATLAB plots
+
+
+%% basic plots
+
+% plot
+x = -180:20:180;
+y = sind( x );
+plot( x, y, 'ro-' );  % see 'help plot' for variants of 'ro-'
+xlabel 'theta'
+ylabel 'sin(theta)'
+title 'a sine wave'
+axis([ -180 180 -1.1 1.1 ]);
+
+% opening, closing, and clearing figure windows
+clf;
+figure;
+figure(5);
+close;
+close all;
+
+% more information on 2D and 3D plots
+help graph2d
+help graph3d
+
+% see also:  hist, bar, errorbar, loglog
+
+
+%% multiple plots on a single set of axes
+
+x = linspace(-pi,pi,10);
+y = sin( x );
+plot( x, y, 'ro-' );
+
+hold on;
+plot( x, 0.5*y, 'b-' );
+plot( x, 0.4*y, 'go' );
+hold off;
+
+axis([ -pi pi -1.1 1.1 ]);
+xlabel 'abscissa'
+ylabel 'ordinate'
+title 'a sampled sine wave'
+legend('sine wave 1','sine wave 2','Location','NorthWest');
+
+zoom on;
+
+
+%% handle graphics
+
+x = linspace(-pi,pi,10);
+y = sin( x );
+h = plot( x, y, 'ro-' );
+
+set( h, 'LineWidth', 3, 'MarkerSize', 10, 'MarkerEdgeColor', 'g', ...
+    'MarkerFaceColor', 'g' );  % set some line properties
+
+get( h )           % see what properties the axes have
+set( h, 'XScale')  % see what values the XScale property could be set to
+
+set( gca, 'XLim', [ -pi pi ], 'YLim', [ -1.1 1.1 ] );  % set the axis limits
+set( gca, 'FontName', 'helvetica', 'FontWeight', 'bold', 'FontSize', 18 );  % set the font
+xlabel 'abscissa'
+ylabel 'ordinate'
+title 'a sampled sine wave'
+
+
+%% plotting a function
+
+% plotting an m-file function
+subplot(1,2,1);
+
+fplot( @sin, [ -pi pi ] );
+
+h = get( gca, 'Children' );
+set( h, 'LineWidth', 3 );
+set( gca, 'FontName', 'helvetica', 'FontWeight', 'bold', 'FontSize', 18 );
+xlabel 'abscissa'
+ylabel 'ordinate'
+axis square
+
+% plotting an inline function
+subplot(1,2,2);
+
+myfunc = @( x ) 0.5 + 0.5*sin( 2*x );
+fplot( myfunc, [ -pi pi ] );
+
+set( gca, 'FontName', 'helvetica', 'FontWeight', 'bold', 'FontSize', 18 );
+xlabel 'abscissa'
+ylabel 'ordinate'
+axis square
+
+
+%% printing and saving plots
+
+print -dtiff sine.tif            % save as .tif
+print -dtiff -r600 sine600.tif   % save as higher-resolution .tif
+print -depsc2 sine.eps           % save as encapsulated postscript
+saveas(gcf,'sine');              % save as matlab figure
+open( 'sine.fig' );              % re-open the matlab figure (can also just double click)
+% print                          % print it on a printer
+
+
+%% 3D plots
+
+xy = linspace(-pi,pi,50);
+[x,y] = meshgrid(xy,xy);
+
+z = sin( x ) .* cos( 2*y );
+surf( x, y, z );
+
+xlabel 'x-axis'
+ylabel 'y-axis'
+zlabel 'z-axis'
+title 'a sinusoidal surface'
+
+rotate3d on                     % switch on interactive rotation mode
+set(gca,'View',[ 45 60 ])       % set a specific viewpoint
+colorbar;                       % show a colour bar
+
+% other 3D plot commands:  mesh, plot3, contour3
