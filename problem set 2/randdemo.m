963778a33a73ccf2275d67c666175c0266179153
Richard Murray <rfm@yorku.ca>
2019-10-24 09:55:53 -0400

    posted problem set 2

---
+++ problem set 2/randdemo.m
@@ -0,0 +1,37 @@
+% randdemo.m  Illustrate how to use the randpos function to get a
+%             random trajectory
+
+clear; clc; clf;
+
+% initialize randpos
+randpos( 'init' );
+
+% prepare figure
+figure(1);
+hold on;
+axis( [ -1 1 -1 1 ] );
+
+% choose trial duration
+tau = 10;
+
+% plot trajectory over time 0 to tau seconds
+tic;
+while 1
+    
+    % get current position
+    [ x, y ] = randpos( toc, tau );
+    plot( x, y, 'ro' );
+    drawnow;
+    
+    % check the time
+    if toc > tau
+        break
+    end
+    
+end
+
+% In the actual experiment, don't have the dot leave a trail.  I'm just
+% doing that here so that we can see the shape of the whole trajectory.
+% 
+% Also, program the experiment using the Psychophysics Toolbox, not a
+% MATLAB figure window as I'm doing here.
