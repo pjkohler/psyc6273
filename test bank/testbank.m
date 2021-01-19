158eaf186d4b667c835138580bdad4b9ab52e711
Peter J. Kohler <pjkohl3r@gmail.com>
2021-01-19 09:48:24 -0500

    test bank example questions

---
+++ test bank/testbank.m
@@ -0,0 +1,55 @@
+% testbank.m
+
+% 1.  Make x equal to a matrix that has the numbers 100 to 200 in the first
+% row and 300 to 400 in the second row.
+
+% 2.  Make x equal to a matrix that has the numbers 1 to 20 in the first
+% column and 21 to 40 in the second column.
+
+% 3.  Make x equal to a 4 x 6 matrix of random numbers uniformly drawn from
+% the interval between 10 and 15.
+
+% 4.  In the following questions, use a matrix m that is a 20 x 5 matrix of
+% normally distributed random numbers.  Resample a new matrix m for each
+% question, e.g., don't let any changes to the matrix m in part (a) affect
+% the matrix m that you use in part (b).
+
+% 4a.  Make x equal to the first row of m.
+
+% 4b.  Make x equal to the last column of m.
+
+% 4c.  Suppose you didn't know the size of matrix m.  Set a equal to the
+% number of rows in m, and b equal to the number of columns.
+
+% 4d.  Suppose you didn't know the size of matrix m.  Set c equal to the
+% number of elements in m.
+
+% 4e.  Make x a row vector with elements that are the maximum of each row
+% of m.
+
+% 4f.  Make x equal to all the elements of m that are greater than zero.
+
+% 4g.  Make x equal to all the elements of m that are less than -1 or
+% greater than 1.
+
+% 4h.  Make x equal to one if all elements of m are greater than zero, and
+% zero otherwise.
+
+% 4i.  Set any elements of m in the third column that are less than zero to
+% NaN.
+
+% 4j.  Set any rows of m that have a value less than -1.5 to NaN.
+
+% 4k.  Set x to the number of elements in m that are greater than 2.
+
+% 4l.  Delete the fourth row of m.
+
+% 4m.  Set the fourth column of m to zero in rows where the third column is
+% less than zero.
+
+% 4n.  Set x to the rows of m that are less than zero in the third column.
+
+% 4o.  Set f equal to the linear indices of the elements of m that are less
+% than zero.
+
+% 4p.  Use find() to set the elements of m that are less than zero to NaN.
