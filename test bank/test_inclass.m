0c0bc690e30f71e8665a8ee4e1c0944adc1e23af
Peter J. Kohler <pjkohl3r@gmail.com>
2021-02-09 08:20:22 -0500

    test 2

--- test bank/test_inclass.m
+++ test bank/test_inclass.m
@@ -43,3 +43,39 @@ c = numel( m );
 x = mean( m, 2 )'; 
 % OR
 x = mean( m' );
+
+%% TEST 2 STARTS HERE
+
+% 4f.  Redefine m (see above). Make x equal to the number of elements of m smaller than -1.5
+
+% 4g.  Make x equal to all the elements of m that are less than -1 or
+% greater than .5
+
+% 4h.  Make x equal to one if any elements are between -.01 and .01 and 0 if
+% no elements are in that range
+
+% 4i.  Set any elements of m in the second row that are bigger than 1 to
+% NaN.
+
+% 4j. Redefine m. Make x equal to the number of columns whose sum is bigger than .5
+
+% 4k. Delete the second, fifth and tenth column of m.
+
+% 4l.  Redefine m. Set f equal to the linear indices of the elements of m
+% that are bigger than .25.
+
+% 4m.  Use f to set the elements of m that are less than zero to NaN.
+
+% 5.  Write a function called test_remain that takes two input arguments a and b, 
+% returns the remainder after dividing a by b if the inputs are both
+% positive, and NaN otherwise. 
+
+% 6.  Write a function called test_randomize that takes any two-dimensional
+% matrix as input, and returns a matrix of the same size, but with the
+% values randomly shuffled. Bonus if you can make the function handle
+% higher-dimensional matrices
+
+% 7.  Write a for loop that prints any number between 1 and 1000 that is divisible by 9 
+
+% 8.  Use a while loop to populate a variable prime_list containing 
+% the first 8 prime numbers bigger than 100
